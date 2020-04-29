7dfd98301901ca549e55f88f843d88da87aa1850
arllainm <arllain@gmail.com>
2020-04-29 09:11:37 -0300

    Estrutura de toast

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import SignIn from './pages/Signin';
 // import SignUp from './pages/Signup';
 import GlobalStyle from './styles/global';
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
